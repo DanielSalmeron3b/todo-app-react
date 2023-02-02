d12e9d27ddd780dafd1759f5411c71a55ef54c60
DanielSalmeron3b <danielesajr@gmail.com>
2023-02-01 21:14:16 -0600

    Trying inline styles

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+const someStyles = {
+    color: 'blue',
+    backgroundColor: 'pink',
+};
+
 function TodoCounter(){
     return (
-    <h2>Has completado 2 de 3 ToDos</h2>
+    <h2 style={someStyles} >Has completado 2 de 3 ToDos</h2>
     );
 };
 
