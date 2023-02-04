0279689542e7d0511e20edda5b0fd93ac5c49127
DanielSalmeron3b <danielesajr@gmail.com>
2023-02-03 22:45:07 -0600

    Learning about events on React

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,17 @@ import React from "react";
 import './CreateTodoButton.css';
 
 function CreateTodoButton(){
+    const onClickButton = () => {
+        alert('Here is where the modal should be shown');
+    }
+
     return (
-        <button className="create-todo-button">+</button>
+        <button 
+            className="create-todo-button"
+            onClick={onClickButton}
+        >
+            +
+        </button>
     );
 }
 
