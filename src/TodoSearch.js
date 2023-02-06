a8f08b924ed275fd900fa71a8f4f4cde7ac32ca6
DanielSalmeron3b <danielesajr@gmail.com>
2023-02-05 22:54:41 -0600

    Learning about the state on React

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,17 +2,20 @@ import React from "react";
 import './TodoSearch.css';
 
 function TodoSearch(){
+    const [patito, setPatito] = React.useState('Juan');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
     };
 
-    return (
-        <input 
+    return [
+        <input
             className="todo-search-input" 
             placeholder="Search a ToDo..."
-            onChange={onSearchValueChange}
-        />
-    );
+            onChange={() => setPatito('Daninel')}
+        />,
+        <p>{patito}</p>
+    ];
 };
 
 export { TodoSearch };
